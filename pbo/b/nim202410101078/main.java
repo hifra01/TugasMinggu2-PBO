aaad88f26a5f37d5e4092ed0b2b917ff9a7b9c7a
ririntrs <89627882+ririntrs@users.noreply.github.com>
2021-09-12 13:39:52 +0700

    Update main.java

--- pbo/b/nim202410101078/main.java
+++ pbo/b/nim202410101078/main.java
@@ -10,15 +10,15 @@ public class main {
 
 //    public static void main(String[] args) {
 //        celcius celcius2 = new celcius(150);
-//        System.out.println(celcius2.toFahreinhet());
-//        System.out.println(celcius2.toKelvin());
-//        System.out.println(celcius2.toReamur());
+//        System.out.println("Konversi Celcius Ke Fahrenheit = " + celcius2.toFahreinhet());
+//        System.out.println("Konversi Celcius Ke Kelvin = " +celcius2.toKelvin());
+//        System.out.println("Konversi Celcius Ke Reamur = " +celcius2.toReamur());
 //    }
 
 
     public static void main(String[] args) {
         balok blk = new balok(12, 7, 5);
-        System.out.println(blk.hitungLuasPermukaan());
-        System.out.println(blk.hitungVolume());
+        System.out.println("Luas Permukaan Balok = " + blk.hitungLuasPermukaan());
+        System.out.println"Volume Balok = " + (blk.hitungVolume());
     }
 }
