cabd972c1f9c64d9957c5b6dc2724f6061d92f2d
Aurelials <90548251+Aurelials@users.noreply.github.com>
2021-09-13 23:20:53 +0700

    Create Bangunruang.java

---
+++ src/pbo/b/nim202410101033/Bangunruang.java
@@ -0,0 +1,20 @@
+//Membuat Class Bangun Ruang (Kubus) 
+
+//1. Membuat package dengan nama pbo.b.nim202410101033
+package pbo.b.nim202410101033;
+
+//2. Membuat class dengan nama rumuskubus
+public class rumuskubus{
+//3. Membuat rumus untuk menghitung volume kubus
+  double sisi;
+  rumuskubus (double sisi){
+    this.sisi;
+}
+  double Volume(){
+    return sisi * sisi * sisi;
+}
+//5. Membuat rumus untuk menghitung luas permukaan kubus
+  double Luaspermukaan(){
+    return 6 * sisi * sisi;
+    }
+}
