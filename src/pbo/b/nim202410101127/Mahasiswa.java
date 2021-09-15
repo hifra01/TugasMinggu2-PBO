0e1712d594c9548321d5a66d2e4d26befd8ca216
crnatiyar <careninatyr@gmail.com>
2021-09-15 18:16:52 +0700

    add folder nim 1127, make class Celcius

--- src/pbo/b/nim202410101127/Mahasiswa.java
+++ src/pbo/b/nim202410101127/Mahasiswa.java
@@ -1,4 +1,12 @@
 package pbo.b.nim202410101127;
 
 public class Mahasiswa {
+    String Nama;
+    String nim;
+    String prodi;
+
+    public void perkenalan(String Nama, String nim, String prodi){
+        System.out.println("Halo, Nama saya "+ Nama + " nim " + nim + " prodi " + prodi);
+        return;
+    }
 }
