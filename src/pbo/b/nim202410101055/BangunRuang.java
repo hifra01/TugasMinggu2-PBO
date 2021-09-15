90c4f6deb78b56c93229666a0af5c9f5ce7ddd2d
alfianurizki <alfiannr16@gmail.com>
2021-09-15 13:06:50 +0700

    Tugas PBO Pr 2 Done

--- src/pbo/b/nim202410101055/BangunRuang.java
+++ src/pbo/b/nim202410101055/BangunRuang.java
@@ -1,4 +1,34 @@
 package pbo.b.nim202410101055;
 
 public class BangunRuang {
+    private double length;
+    private double wide;
+    private double height;
+
+    BangunRuang(double length, double wide, double height) {
+        this.length = length;
+        this.wide = wide;
+        this.height = height;
+    }
+    public double getLength() {
+        return length;
+    }
+    public double getWide() {
+        return wide;
+    }
+    public double getHeight() {
+        return height;
+    }
+    double surface() {
+        return (2 * (length * wide) + 2 * (length * height) + 2 * (wide * height));
+    }
+    double volume() {
+        return ((length * wide) * height);
+    }
+    double total_length_side() {
+        return ((length + wide + height) * 4);
+    }
+    public static void main(String[] args) {
+
+    }
 }
